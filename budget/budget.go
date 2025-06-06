a1b0a791216397d9c3e995cfbf80df18b091ba29
Alexander Budde <alexander.budde@transferwise.com>
2025-06-06 21:10:39 +0000

    Update budget.go

--- budget/budget.go
+++ budget/budget.go
@@ -1,7 +1,7 @@
 package main
 import "fmt"
 func main() {
-	var gross, tax, pension_rate, pension_extra, investments, rent, food, bills, debt, net, pension_total, left float32
+	var gross, tax, pension_rate, pension_extra, investments, rent, household, bills, debt, net, pension_total, left float32
 	fmt.Print("Gross salary: ")
 	fmt.Scan(&gross)
 	fmt.Print("Tax rate(percentage): ")
@@ -14,8 +14,8 @@ func main() {
 	fmt.Scan(&investments)
 	fmt.Print("Rent: ")
 	fmt.Scan(&rent)
-	fmt.Print("Food: ")
-	fmt.Scan(&food)
+	fmt.Print("Household expenses: ")
+	fmt.Scan(&household)
 	fmt.Print("Bills: ")
 	fmt.Scan(&bills)
 	fmt.Print("Debts: ")
@@ -23,5 +23,5 @@ func main() {
 	net = gross * (1 - (tax / 100))
 	pension_total = pension_extra + (gross * (pension_rate / 100))
 	left = net - pension_total - investments - rent - food - bills - debt
-	fmt.Printf("Your net income is %.2f€.\nAfter paying:\n%.2f€ into your pensions,\n%.2f€ in investments\n%.2f€ in rent,\n%.2f€ for food,\n%.2f€ in bills,\n%.2f€ into debts.\nYou have %.2f€ left.\n", net, pension_total, investments, rent, food, bills, debt, left)
+	fmt.Printf("Your net income is %.2f€.\nAfter paying:\n%.2f€ into your pensions,\n%.2f€ in investments\n%.2f€ in rent,\n%.2f€ for food,\n%.2f€ in bills,\n%.2f€ into debts.\nYou have %.2f€ left.\n", net, pension_total, investments, rent, household, bills, debt, left)
 }
