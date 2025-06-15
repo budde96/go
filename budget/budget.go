20238c077b65477660e5f1b2ebb7566d3529903a
Alexander Budde <alexander.budde@transferwise.com>
2025-06-15 06:22:03 +0000

    Update budget.go

--- budget/budget.go
+++ budget/budget.go
@@ -1,17 +1,33 @@
 package main
 import "fmt"
+func excal(g float64) float64 {
+	if g <= 1200 || 654-(0.72667*(g-1200)) >= g*0.22 {
+		return g*0.22
+	} else if g > 1200 && g < 2100 {
+		return 654-(0.72667*(g-1200))
+	} else {
+		return 0
+	}
+}
+func netcal(e, g float64) float64 {
+	return e+(g*0.78)
+}
+func ptotal(pe, g, pr float64) float64 {
+	return pe+(g*(pr/100))
+}
+func left(n, pt, i, r, h, b, d float64) float64 {
+	return n-pt-i-r-h-b-d
+}
 func main() {
-	var gross, tax, pension_rate, pension_extra, investments, rent, household, bills, debt, net, pension_total, left float32
-	fmt.Print("Gross salary: ")
+	var gross, exemption, pension_rate, pension_extra, invsav, rent, household, bills, debt, net, pension_total, remaining float64
+	fmt.Print("Gross income(monthly): ")
 	fmt.Scan(&gross)
-	fmt.Print("Tax rate(percentage): ")
-	fmt.Scan(&tax)
 	fmt.Print("Second pillar(percentage): ")
 	fmt.Scan(&pension_rate)
 	fmt.Print("Third pillar: ")
 	fmt.Scan(&pension_extra)
-	fmt.Print("Investments: ")
-	fmt.Scan(&investments)
+	fmt.Print("Investments/Savings: ")
+	fmt.Scan(&invsav)
 	fmt.Print("Rent: ")
 	fmt.Scan(&rent)
 	fmt.Print("Household expenses: ")
@@ -20,8 +36,9 @@ func main() {
 	fmt.Scan(&bills)
 	fmt.Print("Debts: ")
 	fmt.Scan(&debt)
-	net = gross * (1 - (tax / 100))
-	pension_total = pension_extra + (gross * (pension_rate / 100))
-	left = net - pension_total - investments - rent - food - bills - debt
-	fmt.Printf("Your net income is %.2f€.\nAfter paying:\n%.2f€ into your pensions,\n%.2f€ in investments\n%.2f€ in rent,\n%.2f€ for food,\n%.2f€ in bills,\n%.2f€ into debts.\nYou have %.2f€ left.\n", net, pension_total, investments, rent, household, bills, debt, left)
+	exemption = excal(gross)
+	net = netcal(exemption, gross)
+	pension_total = ptotal(pension_extra, gross, pension_rate)
+	remaining = left(net, pension_total, invsav, rent, household, bills, debt)
+	fmt.Printf("------\nYour net income is %.2f€.\nAfter paying:\n%.2f€ into your pensions,\n%.2f€ in investments/savings\n%.2f€ in rent,\n%.2f€ for food,\n%.2f€ in bills,\n%.2f€ into debts.\nYou have %.2f€ left.\n", net, pension_total, invsav, rent, household, bills, debt, remaining)
 }
