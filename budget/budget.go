47329e2feba838f21b6ea5d0aa1931fa3616a6c1
Alexander Budde <alexander.budde@transferwise.com>
2025-06-15 12:08:53 +0000

    Update budget.go

--- budget/budget.go
+++ budget/budget.go
@@ -1,5 +1,5 @@
 package main
-import "fmt"
+import	"fmt"
 func input() (float64, float64, float64, float64, float64, float64, float64, float64) {
 	var g, pr, pe, is, r, h, b, d float64
 	fmt.Print("Gross income(monthly): ")
@@ -25,8 +25,8 @@ func input() (float64, float64, float64, float64, float64, float64, float64, flo
 	}
 }
 func excal(g float64) float64 {
-	if g <= 1200 || 654-(0.72667*(g-1200)) >= g*0.22 {
-		return g*0.22
+	if g <= 1200 {
+		return 654
 	} else if g > 1200 && g < 2100 {
 		return 654-(0.72667*(g-1200))
 	} else {
@@ -34,7 +34,7 @@ func excal(g float64) float64 {
 	}
 }
 func netcal(e, g float64) float64 {
-	return e+(g*0.78)
+	return e+((g-e)*0.78)
 }
 func ptotal(pe, g, pr float64) float64 {
 	return pe+(g*(pr/100))
@@ -46,6 +46,7 @@ func main() {
 	gross, pension_rate, pension_extra, invsav, rent, household, bills, debt := input()
 	exemption := excal(gross)
 	net := netcal(exemption, gross)
+	fmt.Println((gross-exemption)*0.22)
 	pension_total := ptotal(pension_extra, gross, pension_rate)
 	remaining := left(net, pension_total, invsav, rent, household, bills, debt)
 	fmt.Printf("------\nYour net income is %.2f€.\nAfter paying:\n%.2f€ into your pensions,\n%.2f€ in investments/savings\n%.2f€ in rent,\n%.2f€ for food,\n%.2f€ in bills,\n%.2f€ into debts.\nYou have %.2f€ left.\n", net, pension_total, invsav, rent, household, bills, debt, remaining)
