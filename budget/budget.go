b6958c6ad15d7674094ddb189bceaddb6cc809d9
Alexander Budde <alexander.budde@transferwise.com>
2025-06-15 06:32:27 +0000

    Update budget.go

--- budget/budget.go
+++ budget/budget.go
@@ -1,5 +1,25 @@
 package main
 import "fmt"
+func input() (float64, float64, float64, float64, float64, float64, float64, float64) {
+	var g, pr, pe, is, r, h, b, d float64
+	fmt.Print("Gross income(monthly): ")
+	fmt.Scan(&g)
+	fmt.Print("Second pillar(percentage): ")
+	fmt.Scan(&pr)
+	fmt.Print("Third pillar: ")
+	fmt.Scan(&pe)
+	fmt.Print("Investments/Savings: ")
+	fmt.Scan(&is)
+	fmt.Print("Rent: ")
+	fmt.Scan(&r)
+	fmt.Print("Household expenses: ")
+	fmt.Scan(&h)
+	fmt.Print("Bills: ")
+	fmt.Scan(&b)
+	fmt.Print("Debts: ")
+	fmt.Scan(&d)
+	return g, pr, pe, is, r, h, b, d
+}
 func excal(g float64) float64 {
 	if g <= 1200 || 654-(0.72667*(g-1200)) >= g*0.22 {
 		return g*0.22
@@ -19,26 +39,10 @@ func left(n, pt, i, r, h, b, d float64) float64 {
 	return n-pt-i-r-h-b-d
 }
 func main() {
-	var gross, exemption, pension_rate, pension_extra, invsav, rent, household, bills, debt, net, pension_total, remaining float64
-	fmt.Print("Gross income(monthly): ")
-	fmt.Scan(&gross)
-	fmt.Print("Second pillar(percentage): ")
-	fmt.Scan(&pension_rate)
-	fmt.Print("Third pillar: ")
-	fmt.Scan(&pension_extra)
-	fmt.Print("Investments/Savings: ")
-	fmt.Scan(&invsav)
-	fmt.Print("Rent: ")
-	fmt.Scan(&rent)
-	fmt.Print("Household expenses: ")
-	fmt.Scan(&household)
-	fmt.Print("Bills: ")
-	fmt.Scan(&bills)
-	fmt.Print("Debts: ")
-	fmt.Scan(&debt)
-	exemption = excal(gross)
-	net = netcal(exemption, gross)
-	pension_total = ptotal(pension_extra, gross, pension_rate)
-	remaining = left(net, pension_total, invsav, rent, household, bills, debt)
+	gross, pension_rate, pension_extra, invsav, rent, household, bills, debt := input()
+	exemption := excal(gross)
+	net := netcal(exemption, gross)
+	pension_total := ptotal(pension_extra, gross, pension_rate)
+	remaining := left(net, pension_total, invsav, rent, household, bills, debt)
 	fmt.Printf("------\nYour net income is %.2f€.\nAfter paying:\n%.2f€ into your pensions,\n%.2f€ in investments/savings\n%.2f€ in rent,\n%.2f€ for food,\n%.2f€ in bills,\n%.2f€ into debts.\nYou have %.2f€ left.\n", net, pension_total, invsav, rent, household, bills, debt, remaining)
 }
